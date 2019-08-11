390157b9505ace171a0e1bb0db09cc0e3f37cddc
Kent Sandvik <ksandvik@roku.com>
2019-08-11 16:43:15 -0700

    first fish config

---
+++ config.fish
@@ -0,0 +1,38 @@
+# GENERAL
+abbr l 'exa -l'
+
+abbr -a c cargo
+abbr -a e nvim
+
+# GIT
+abbr -a gd 'git diff'
+abbr -a g git
+
+# AUTOJUMP
+begin
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
+
+
+function fish_prompt
+	set_color black
+	echo -n "["(date "+%y.%m.%d")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color black
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '~> '
+	set_color normal
+end
+
+
