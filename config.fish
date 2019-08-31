c24e6aaf7e477e4d8eee5c2670744595db144a8b
K Sandvik <sandvik@gmail.com>
2019-08-30 20:03:47 -0700

    posix-source to read environment variables

--- config.fish
+++ config.fish
@@ -62,3 +62,11 @@ function gitpull
         cd ..
     end
 end
+
+# Read environment Variables
+function posix-source
+	for i in (cat $argv)
+		set arr (echo $i |tr = \n)
+  		set -gx $arr[1] $arr[2]
+	end
+end
