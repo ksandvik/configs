00679515c32a44ebf758f5e8148ac9cb96e9d3d2
K Sandvik <sandvik@gmail.com>
2021-04-13 17:13:33 -0700

    yt audio downloads

--- config.fish
+++ config.fish
@@ -21,6 +21,7 @@ abbr -a lt 'ls -lt | sed 5q'
 abbr -a el 'ls -tt | sed 1q | xargs $EDITOR'
 abbr -a today 'date '+%Y-%m-%d''
 abbr -a ri 'rg -i '
+abbr -a ytaudio 'youtube-dl -x --audio-format mp3 --prefer-ffmpeg "'
 
 # GIT
 abbr -a gd 'git diff'
