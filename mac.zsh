e2bdf3898330e3f563185ce536252cf78199d47a
Kent Sandvik <ksandvik@gqcgj3g4wn.attlocal.net>
2022-09-30 16:15:39 -0700

    fixing brew setups

--- mac.zsh
+++ mac.zsh
@@ -1,23 +1,26 @@
-# Mac related ZSH settings
+# MAC RELATED CONFIGS
+
+# HOMEBREW
+eval "$(/opt/homebrew/bin/brew shellenv)"
+# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 
 # Additional Mac paths
 export PATH=~/bin:/usr/local/bin:$PATH:.:/opt/local/bin:/opt/bin
 
 # Set CLICOLOR if you want Ansi Colors in iTerm2
 export CLICOLOR=1
+
 # Set colors to match iTerm2 Terminal Colors
 export TERM=xterm-256color
 
 # EMACS
-
 PATH=/Users/ksandvik/.emacs.d/bin:$PATH
 
 function updateBrew {
 	brew update && brew upgrade && brew cleanup
 }
-
-# Shortcuts to Mac Applications
-alias saf="open /Applications/Safari.app"
 
 # Search using Spotlight
 alias saf="open /Applications/Safari.app"
