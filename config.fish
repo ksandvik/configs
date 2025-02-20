0fbb4dabc67bdeee26ab806d5cdba2b395b270fa
Kent Sandvik <sandvik@mac.com>
2025-02-20 15:04:24 -0800

    filesize abbr

--- config.fish
+++ config.fish
@@ -57,6 +57,7 @@ abbr -a q 'task end.after:today completed ; task list'
 abbr -a saf 'open /Applications/Safari.app'
 abbr -a rd 'random-markdown-file.py ddn'
 abbr -a ra 'random-markdown-file.py'
+abbr -a filesize 'du -hcd2 .'
 
 
 # GIT
