52cf396ced14bdb6436ab717403452825d5407fc
Kent Sandvik <ksandvik@dotsandblobs.com>
2025-04-30 11:00:31 -0700

    no python venv

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@
 # GLOBAL
 set -gx TERM xterm-256color
 set -gx fish_term24bit 1.
-set -gx EDITOR zed
+set -gx EDITOR code
 
 set -gx FZF_DEFAULT_COMMAND rg --files
 set -gx FZF_DEFAULT_OPTS -m --height 50% --border --layout=reverse
@@ -281,32 +281,6 @@ function multicd
 end
 
 abbr --add dotdot --regex '^\.\.+$' --function multicd
-
-# Python venv
-
-# function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-#   status --is-command-substitution; and return
-
-#   # Check if we are inside a git directory
-#   if git rev-parse --show-toplevel &>/dev/null
-#     set gitdir (realpath (git rev-parse --show-toplevel))
-#     set cwd (pwd)
-#     # While we are still inside the git directory, find the closest
-#     # virtualenv starting from the current directory.
-#     while string match "$gitdir*" "$cwd" &>/dev/null
-#       if test -e "$cwd/.venv/bin/activate.fish"
-#         source "$cwd/.venv/bin/activate.fish" &>/dev/null
-#         return
-#       else
-#         set cwd (path dirname "$cwd")
-#       end
-#     end
-#   end
-#   # If virtualenv activated but we are not in a git directory, deactivate.
-#   if test -n "$VIRTUAL_ENV"
-#     deactivate
-#   end
-# end
 
 
 # fish
