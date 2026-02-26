fb9aa7715f13e467e01cdefdfe7cdaa930ec50f1
Kent Sandvik <ksandvik@dotsandblobs.com>
2026-02-25 16:25:13 -0800

    ml change

--- config.fish
+++ config.fish
@@ -65,7 +65,8 @@ abbr -a ra 'random-markdown-file.py'
 abbr -a filesize 'du -hcd2 .'
 abbr -a b bat
 abbr -a killfinder 'killall Finder'
-abbr -a ml ' nvim /Users/ksandvik/ic/KSNotes/Log/Log.md'
+# abbr -a ml ' nvim /Users/ksandvik/ic/KSNotes/Log/Log.md'
+abbr -a ml 'open /Users/ksandvik/ic/KSNotes/Log/Log.md'
 abbr -a cl ' glow -p /Users/ksandvik/ic/KSNotes/Log/Log.md'
 
 # GIT
